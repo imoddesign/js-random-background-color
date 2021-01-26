666848d03bb93346574c252e28110220863ded5a
hhvvmm <imoddesigner@gmail.com>
2021-01-26 11:12:19 +0700

    try to understand type 2

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 const app = document.getElementById('app');
 const colorDiv = document.createElement('h1');
+const button = document.createElement('button');
 
-function randomBackgroundColor() {
-  const result = [];
-  for(i = 0; i < 255; i++) {
-    result.push(i);
-  }
-  const red = Math.floor(Math.random(result) * result.length)
-  const green = Math.floor(Math.random(result) * result.length)
-  const blue = Math.floor(Math.random(result) * result.length)
+button.innerText = "Change Background";
+button.addEventListener('click', randomBackgroundColor);
+app.appendChild(button);
+
+// type 1
+// function randomBackgroundColor() {
+//   const result = [];
+//   for(i = 0; i < 255; i++) {
+//     result.push(i);
+//   }
+//   const red = Math.floor(Math.random(result) * result.length)
+//   const green = Math.floor(Math.random(result) * result.length)
+//   const blue = Math.floor(Math.random(result) * result.length)
   
-  app.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-  app.appendChild(colorDiv).innerText = `rgb(${red}, ${green}, ${blue})`;
+//   app.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+//   app.appendChild(colorDiv).innerText = `rgb(${red}, ${green}, ${blue})`;
+// }
+
+// type 2
+const hexCodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+function randomBackgroundColor() {
+  let hex = '#';
+  for (let i = 0; i < 6; i++) {
+    const index = Math.floor(Math.random() * hexCodes.length);
+    hex += hexCodes[index];
+  }
+  app.appendChild(colorDiv).textContent = hex;
+  app.style.backgroundColor = hex;
 }
 
 randomBackgroundColor();
